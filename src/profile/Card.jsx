e0c07a6abca24c9241059ae85bdfc1ec61fcad47
shabbir789ahmad <58200046+shabbir789ahmad@users.noreply.github.com>
2022-06-04 09:48:41 +0500

    Update Card.jsx

--- src/profile/Card.jsx
+++ src/profile/Card.jsx
@@ -13,7 +13,7 @@ export default function Card({id,title,img}){
         <div className="item item_hover">
                   <div className="card shadow">
                     <div className="card-body w-100 p-0">
-                      <img src={img} />
+                      <img src={img} alt="image"/>
                       <h5 className="mt-2">{title}</h5>
                       <p className="mb-0">hffffghfttjhkh</p>         
                      </div>
