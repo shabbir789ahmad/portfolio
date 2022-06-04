50af19cc6ab8c75442da094c7296acff88fa73e8
shabbir789ahmad <58200046+shabbir789ahmad@users.noreply.github.com>
2022-06-04 09:48:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+
 
 import './App.css';
 import Navbar from './profile/Navbar';
 import Banner from './profile/Banner';
 import Portfolio from './profile/Portfolio';
-import Works from './profile/Works';
+
 import Testimonial from './profile/Testimonial';
 import Contact from './profile/Contact';
-import {BrowserRouter, Routes ,Route} from "react-router-dom";
+
 function App() {
   return (
   <>
